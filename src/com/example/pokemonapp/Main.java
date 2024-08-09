c613539023db88d6e4b662ecfe2b7e1e7ad441e5
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-08-10 08:45:08 +0900

    ポケモン選択の表示

---
+++ src/com/example/pokemonapp/Main.java
@@ -0,0 +1,18 @@
+package com.example.pokemonapp;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("ポケモンを選んで下さい。");
+        System.out.println("1:ニャオハ　2:ホゲータ　3:クワッス");
+        String choice = scanner.nextLine();
+        
+
+    }
+
+}
